b2ed08e61e0cc3bdd389188cc1120d87147aa029
ankur24s <ankursahu64@gmail.com>
2025-11-05 14:04:05 +0530

    upto day3

--- models/gold_airbnb/fact_reviews.sql
+++ models/gold_airbnb/fact_reviews.sql
@@ -1,1 +1,9 @@
+{{config(
+    materialized = 'incremental', 
+    incremental_strategy="append"
+    )}}
+
 select * from  {{ref('silver_reviews')}}
+{% if is_incremental() %}
+where review_date > (select max(review_date) from {{this}})
+{% endif %}
