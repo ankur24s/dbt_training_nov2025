7740d4123eeceee590582e1abe7134915d374ffc
ankur24s <ankursahu64@gmail.com>
2025-11-06 12:32:15 +0530

    updated snapshot

--- models/gold_airbnb/fact_reviews.sql
+++ models/gold_airbnb/fact_reviews.sql
@@ -1,7 +1,14 @@
-{{config(
-    materialized = 'incremental', 
-    incremental_strategy="append"
-    )}}
+{{
+  config(
+    materialized = 'incremental',
+    unique_key = ['listing_id','reviewer_name'],
+    cluster_by = ['REVIEW_DATE'],  
+    incremental_strategy = 'merge',
+    incremental_predicates = [
+      "DBT_INTERNAL_DEST.REVIEW_DATE > dateadd(day, -30, '2021-10-28')"
+    ]
+  )
+}}
 
 select * from  {{ref('silver_reviews')}}
 {% if is_incremental() %}
